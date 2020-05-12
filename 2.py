069e699d543ab6d9b21d5cc00f5a7ed7cf707db8
KonTroAl <kostya98295@mail.ru>
2020-05-12 22:52:01 +0500

    Final version of Task2
    Код получился немного громоздкий, зато при каждом варианте соблюдается форматирование текста

--- 2.py
+++ 2.py
@@ -1,7 +1,23 @@
 sec = int(input("Enter time in seconds: "))
 hour = sec // 3600
-print(hour)
 m = sec % 3600 // 60
-print(m)
 s = sec % 3600 % 60
-print(s)
+
+if 10 > hour >= 0:
+    if 10 > m >= 0:
+        if 10 > s >= 0:
+            print(f"0{hour}:0{m}:0{s}")
+        else:
+            print(f"0{hour}:0{m}:{s}")
+    else:
+        print(f"0{hour}:{m}:{s}")
+
+
+if hour >= 10:
+    if 10 > m >= 0:
+        if 10 > s >= 0:
+            print(f"{hour}:0{m}:0{s}")
+        else:
+            print(f"{hour}:0{m}:{s}")
+    else:
+        print(f"{hour}:{m}:{s}")
