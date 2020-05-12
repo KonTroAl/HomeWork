95e1b8d0d9efbecc1ce2a5cec9c20356e5eb648e
KonTroAl <kostya98295@mail.ru>
2020-05-12 12:45:43 +0500

    First Task Complit

---
+++ 1.py
@@ -0,0 +1,10 @@
+name = "Konstantin"
+age = 25
+print(f"Hello, my name {name}! I'm {age}.")
+name_2 = input("What's your name? ")
+age_2 = input("How old are you? ")
+print(f"Hello, my name {name_2}! I'm {age_2}.")
+surname_2 = input("What's your surname? ")
+print(surname_2)
+weight = input("Whats' your weight? ")
+print(weight)
