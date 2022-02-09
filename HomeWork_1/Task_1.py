3b4ce04cc364beda6680eaf7abdfff5842df253f
KonTroAl <troshenkin.k@yandex.ru>
2022-02-09 19:34:46 +0500

    task_1 done

--- HomeWork_1/Task_1.py
+++ HomeWork_1/Task_1.py
@@ -1,4 +1,16 @@
 """
 Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя, сохранить JSON-вывод в файле *.json
 """
+import requests
+# from pprint import pprint
 
+username = 'KonTroAl'
+
+url = f'https://api.github.com/users/{username}/repos'
+
+response = requests.get(url)
+j_data = response.json()
+
+print(f'All repositories of {username}: \n')
+for item in j_data:
+    print(item['name'])
