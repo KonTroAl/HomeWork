a6dcb502974c66ff87a311ffe06af0bbe3c2c85e
KonTroAl <troshenkin.k@yandex.ru>
2022-02-16 21:47:07 +0500

    done with task 1 of homework

--- HomeWork_3/main.py
+++ HomeWork_3/main.py
@@ -1,18 +1,22 @@
 """
-Необходимо собрать информацию о вакансиях на вводимую должность (используем input или через аргументы получаем должность)
-с сайтов HH(обязательно) и/или Superjob(по желанию). Приложение должно анализировать несколько страниц сайта (также вводим через input или аргументы).
-Получившийся список должен содержать в себе минимум:
-+ Наименование вакансии.
-+ Предлагаемую зарплату (разносим в три поля: минимальная и максимальная и валюта. Цифры преобразуем к цифрам).
-+ Ссылку на саму вакансию.
-- Сайт, откуда собрана вакансия.
-По желанию можно добавить ещё параметры вакансии (например, работодателя и расположение).
-Структура должна быть одинаковая для вакансий с обоих сайтов. Общий результат можно вывести с помощью dataFrame через pandas. Сохраните в json либо csv.
+1. Развернуть у себя на компьютере/виртуальной машине/хостинге MongoDB и реализовать функцию, которая будет добавлять только новые вакансии/продукты в вашу базу.
+2. Написать функцию, которая производит поиск и выводит на экран вакансии с заработной платой больше введённой суммы (необходимо анализировать оба поля зарплаты).
+Для тех, кто выполнил задание с Росконтролем - напишите запрос для поиска продуктов с рейтингом не ниже введенного или качеством не ниже введенного
+(то есть цифра вводится одна, а запрос проверяет оба поля)
 """
 import requests
-from bs4 import BeautifulSoup
-from pprint import pprint
 import json
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError as dke
+
+PASSWORD = 'Rjcnz2706'
+
+
+client = MongoClient('localhost', 27017)
+db = client['vacancy_db']    # database
+vacancy_collection = db.vacancy        # collection
+
 
 base_url = 'https://hh.ru'
 # vacancy = input('Enter vacancy: ')
@@ -22,8 +26,10 @@ url_f = 'https://hh.ru/search/vacancy?clusters=true&ored_clusters=true&enable_sn
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.27 (Edition Yx 05)'}
 
-params = {'text': vacancy.capitalize(), 'page': 0}
+params = {'text': vacancy.capitalize(), 'page': 0, 'items_on_page': 20}
 result_list = []
+start_id = 0
+count_of_new_vacancy = 0
 while True:
     response = requests.get(base_url + '/search/vacancy', headers=headers, params=params)
     if response.ok:
@@ -33,6 +39,7 @@ while True:
 
         for item in result:
             result_data = {}
+            result_data['_id'] = start_id
             vacancy_name = item.find('a').getText()
             result_data['vacancy_name'] = vacancy_name
 
@@ -45,29 +52,38 @@ while True:
                 salary_list = salary.split()
                 currency = salary_list[-1]
                 result_data['currency'] = currency
+                new_salary = salary.strip().replace('\u202f', '').replace('-', '').split()
                 try:
-                    min_salary = int(salary_list[0] + salary_list[1])
+                    min_salary = int(new_salary[0])
                 except ValueError:
-                    min_salary = int(salary_list[1] + salary_list[2])
-
+                    min_salary = int(new_salary[1])
                 result_data['min_salary'] = min_salary
-                if len(salary_list) > 4:
+                if len(new_salary) > 4:
                     try:
-                        max_salary = int(salary_list[3] + salary_list[4])
+                        max_salary = int(new_salary[3])
                     except ValueError:
-                        max_salary = int(salary_list[2] + salary_list[3])
+                        max_salary = int(new_salary[2])
+
                     result_data['max_salary'] = max_salary
+
             result_data['site'] = 'HeadHunter'
             result_list.append(result_data)
+            start_id += 1
+
+            try:
+                vacancy_collection.insert_one(result_data)
+                count_of_new_vacancy += 1
+            except dke:
+                pass
 
         next_page_div = dom.find('div', {'class': 'pager'})
         next_page = next_page_div.find(text='дальше')
         if next_page:
             params['page'] += 1
         else:
+            print(f'в базу данных добавлено новых вакансий: {count_of_new_vacancy}')
             break
     else:
         break
 
-with open('result.json', 'w') as file:
-    json.dump(result_list, file, indent=4)
+
