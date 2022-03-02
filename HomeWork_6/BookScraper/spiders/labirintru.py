5597b7b69a905de9e7714d0de024bb19b0cb3b21
KonTroAl <troshenkin.k@yandex.ru>
2022-03-02 21:14:31 +0500

    raw data collected

--- HomeWork_6/BookScraper/spiders/labirintru.py
+++ HomeWork_6/BookScraper/spiders/labirintru.py
@@ -1,10 +1,27 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class LabirintruSpider(scrapy.Spider):
     name = 'labirintru'
     allowed_domains = ['labirint.ru']
+    base_url = 'https://www.labirint.ru'
     start_urls = ['https://www.labirint.ru/search/%D1%84%D0%B0%D0%BD%D1%82%D0%B0%D1%81%D1%82%D0%B8%D0%BA%D0%B0/?stype=0&page=1&russianonly=1&available=1&paperbooks=1']
 
-    def parse(self, response):
-        print(response.url)
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath("//div[@class='pagination-next']/a/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+        links = response.xpath("//a[@class='product-title-link']/@href").getall()
+        for link in links:
+            yield response.follow(link, callback=self.book_parse)
+
+    def book_parse(self, response: HtmlResponse):
+        book_title = response.xpath("//h1//text()").get()
+        authors_name = response.xpath("//div[@class='authors']/a/text()").getall()
+        rate = response.xpath("//div[@id='rate']/text()").get()
+        main_price = response.xpath("//div[@class='buying-priceold-val']/span/text() | @class='buying-price']/span[@class='buying-price-val']/text()").get()
+        sale_price = response.xpath("//div[@class='buying-pricenew-val']/span/text()").get()
+
+        print()
