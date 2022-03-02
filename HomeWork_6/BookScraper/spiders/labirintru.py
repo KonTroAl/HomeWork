af6c49782d8e70709ad6bf1530c19a06acad7d17
KonTroAl <troshenkin.k@yandex.ru>
2022-03-02 20:08:35 +0500

    done with settings

--- HomeWork_6/BookScraper/spiders/labirintru.py
+++ HomeWork_6/BookScraper/spiders/labirintru.py
@@ -4,7 +4,7 @@ import scrapy
 class LabirintruSpider(scrapy.Spider):
     name = 'labirintru'
     allowed_domains = ['labirint.ru']
-    start_urls = ['http://labirint.ru/']
+    start_urls = ['https://www.labirint.ru/search/%D1%84%D0%B0%D0%BD%D1%82%D0%B0%D1%81%D1%82%D0%B8%D0%BA%D0%B0/?stype=0&page=1&russianonly=1&available=1&paperbooks=1']
 
     def parse(self, response):
         pass
