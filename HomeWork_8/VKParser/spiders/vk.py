8edec1d9cf909e35b1cfaaf7887c7b00ddd33c73
KonTroAl <troshenkin.k@yandex.ru>
2022-03-26 11:45:30 +0500

    start work

---
+++ HomeWork_8/VKParser/spiders/vk.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class VkSpider(scrapy.Spider):
+    name = 'vk'
+    allowed_domains = ['vk.com']
+    start_urls = ['http://vk.com/']
+
+    def parse(self, response):
+        pass
