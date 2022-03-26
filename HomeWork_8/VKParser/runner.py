983d94f5f2d46784efc81076ec7539140a5329c9
KonTroAl <troshenkin.k@yandex.ru>
2022-03-26 15:26:37 +0500

    first step

--- HomeWork_8/VKParser/runner.py
+++ HomeWork_8/VKParser/runner.py
@@ -2,14 +2,13 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
 import settings
-from HomeWork_7.LeroyMerlinParser.spiders.leroymerlin import LeroymerlinSpider
+from HomeWork_8.VKParser.spiders.vk import VkSpider
 
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
 
     process = CrawlerProcess(settings=crawler_settings)
-    search = 'ковры'
-    process.crawl(LeroymerlinSpider, search=search)
+    process.crawl(VkSpider)
 
     process.start()
--- HomeWork_8/VKParser/spiders/vk.py
+++ HomeWork_8/VKParser/spiders/vk.py
@@ -4,7 +4,7 @@ import scrapy
 class VkSpider(scrapy.Spider):
     name = 'vk'
     allowed_domains = ['vk.com']
-    start_urls = ['http://vk.com/']
+    start_urls = ['https://vk.com/']
 
     def parse(self, response):
         pass
