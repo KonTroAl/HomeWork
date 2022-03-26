03be4b1bc74d85506da2fcb0e8ad8bc76e4342ec
KonTroAl <troshenkin.k@yandex.ru>
2022-03-26 15:28:58 +0500

    try to get access to vk.com

--- HomeWork_8/VKParser/spiders/vk.py
+++ HomeWork_8/VKParser/spiders/vk.py
@@ -1,10 +1,22 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class VkSpider(scrapy.Spider):
     name = 'vk'
     allowed_domains = ['vk.com']
     start_urls = ['https://vk.com/']
+    vk_login_url = ''
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        yield scrapy.FormRequest(
+            self.vk_login_url,
+            method='POST',
+            callback=self.login,
+            formdata={
+                
+            }
+        )
+
+    def login(self, response: HtmlResponse):
+        print()
